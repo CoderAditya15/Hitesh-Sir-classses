150d1bbfd69a0d00cabcb90de27da68dfdb15459
CoderAditya15 <167786607+CoderAditya15@users.noreply.github.com>
2024-06-04 01:15:12 +0000

    conversion of datatype

---
+++ Adi/conversion.js
@@ -0,0 +1,20 @@
+//let score="33"
+//console.log(typeof score)
+//console.log(typeof(score))
+
+//let valueInNumber= Number (score)
+//console.log(typeof valueInNumber)
+
+//let sonali=1
+
+//let sundaram=Boolean(sonali)
+//console.log(typeof sundaram)
+//console.log(typeof sonali)
+//console.log(sundaram)
+
+
+//let chhoti=324
+
+//let sejal=String(chhoti)
+//console.log(typeof chhoti)
+//console.log(typeof sejal)
