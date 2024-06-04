2377657b11336caa6305f92a7f88d6792ca2de16
CoderAditya15 <167786607+CoderAditya15@users.noreply.github.com>
2024-06-04 02:05:38 +0000

    conversion

--- Adi/conversion.js
+++ Adi/conversion.js
@@ -18,3 +18,37 @@
 //let sejal=String(chhoti)
 //console.log(typeof chhoti)
 //console.log(typeof sejal)
+
+
+//let Adi="Sonali"
+
+//let kumar=Number(Adi)
+//console.log(typeof Adi)
+
+//operation
+
+// let value=50
+// let negvalue=-value
+// console.log(negvalue)
+
+// let str1="hello"
+// let str2="Adi"
+
+// let str3=str1+str2
+
+// console.log(str3)
+//console.log(str1+str2)
+
+//let num1,num2,num3
+
+//num1=num2=num3=2+2
+//In coding readability is necessary
+
+//comparison of data types
+
+console.log(2>1);
+console.log(2>=1)
+console.log(2<1)
+console.log(2==1)
+console.log(2!=1)
+
