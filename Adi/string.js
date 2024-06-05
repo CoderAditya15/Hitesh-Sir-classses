8d2f280704ab933558916ce53b33d0240b8d4a38
CoderAditya15 <167786607+CoderAditya15@users.noreply.github.com>
2024-06-05 06:14:26 +0000

    string

---
+++ Adi/string.js
@@ -0,0 +1,13 @@
+const name="hitesh"
+const repoCount=50
+
+//console.log(name + repoCount+"value")
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName=new String('hiteshhc')//this is also a method to define string
+console.log(gameName[0])
+console.log(gameName[1])
+
+console.log(gameName.length)
+
